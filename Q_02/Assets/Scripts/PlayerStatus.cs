ae634e01b26e5f0f07303bff29db33997b88daea
LeeJeongbin97 <ljb97310@naver.com>
2024-11-19 12:11:43 +0900

    [Fix] Status 스크립트 무한재귀 버그 수정

--- Q_02/Assets/Scripts/PlayerStatus.cs
+++ Q_02/Assets/Scripts/PlayerStatus.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
+    private float _moveSpeed; // 값을 저장할 백업 변수
+
     public float MoveSpeed
     {
-        get => MoveSpeed;
-        private set => MoveSpeed = value;
+        /*
+         * get접근자와 set접근자가 둘 다 자기 자신을 참조하고 있어 무한 재귀 발생
+         * _moveSpeed 프로퍼티를 백업 변수로 저장하여 참조하도록 수정 필요
+         */
+        get => MoveSpeed; // get에서 백업 변수 반환
+        private set => MoveSpeed = value; // 백업 변수에 값 설정
     }
 
     private void Awake()
