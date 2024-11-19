76587e0cf8aeb30e0524a1df4c517d9c54b5d925
LeeJeongbin97 <ljb97310@naver.com>
2024-11-19 11:23:48 +0900

    [Fix] Awake 호출 에러
    Start()에서 Cube가 생성되기 이전에
    Awake()에서 Cube Position 값을 설정하는 에러 수정

--- Q_01/Assets/Scripts/CubeManager.cs
+++ Q_01/Assets/Scripts/CubeManager.cs
@@ -9,14 +9,19 @@ public class CubeManager : MonoBehaviour
     private CubeController _cubeController;
     private Vector3 _cubeSetPoint;
 
+    /*
+     * Awake에서 큐브 포지션을 호출하고 있는데 큐브가 아직 Start에서 실행되기 이전 상태이므로 큐브 컨트롤러가 nullreference가 뜨는 상황
+     * 즉 큐브가 생성되기 전에 포지션을 설정하려고 하기 때문에 발생하는 에러
     private void Awake()
     {
         SetCubePosition(3, 0, 3);
     }
+    */
 
     private void Start()
     {
         CreateCube();
+        SetCubePosition(3, 0, 3); // 큐브가 생성된 후 위치 설정을 하기 위해 Awake가 아닌 Start로 이동
     }
 
     private void SetCubePosition(float x, float y, float z)
